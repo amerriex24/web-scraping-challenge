93a4be2a2226babd7884201fddc28a5189d7811e
Alex Merriex <alexrmerriex@gmail.com>
2021-06-09 23:35:29 -0600

    Cleaning up folders

---
+++ Resources/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars as sc
+
+app=Flask(__name__)
+mongo=PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+#create index route 
+@app.route("/")
+def index():
+    mars_scrape=mongo.db.mars_data.find_one()
+    return render_template("index.html", mars_data=sc)
+
+@app.route("/")
+def index():
+    mars_scrape=mongo.db.mars_data.find_one()
+    return render_template("index.html", mars_data=mars_scrape)
+
+@app.route("/scrape")
+
+def scrape():
+    scrape_data=sc.scrape()
+    mongo.db.mars_data.update_many({},{'$set':scrape_data},True)
+    return redirect("/",code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
